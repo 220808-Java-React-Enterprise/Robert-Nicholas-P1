0b7b91cfa824ec379cd46d38a229b1b122eb402e
Nicholas Fielder <46355174+fieldter@users.noreply.github.com>
2022-08-30 15:03:03 -0400

    testing git

---
+++ src/main/java/com/revature/ers/daos/ReimbursementDAO.java
@@ -0,0 +1,29 @@
+package com.revature.ers.daos;
+
+import com.revature.ers.models.Reimbursement;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ReimbursementDAO implements CrudDAO<Reimbursement>{
+
+    @java.lang.Override
+    public void save(Reimbursement obj) throws IOException {
+
+    }
+
+    @java.lang.Override
+    public void update(Reimbursement obj) {
+
+    }
+
+    @java.lang.Override
+    public void delete(String id) {
+
+    }
+
+    @java.lang.Override
+    public List<Reimbursement> getAll() {
+        return null;
+    }
+}
